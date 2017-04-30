b670b5d50c1d18313737e04019439ebde19d1a1b
Jaimin Fojdar <jaiminfojdar@gmail.com>
2017-04-30 13:59:08 +0530

    Slider & Splash added

---
+++ app/src/main/java/com/jaiminf/naturaldisasterreliefapp/welcome_slide2.java
@@ -0,0 +1,13 @@
+package com.jaiminf.naturaldisasterreliefapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcome_slide2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_slide2);
+    }
+}
---
+++ app/src/main/java/com/jaiminf/naturaldisasterreliefapp/welcome_slide4.java
@@ -0,0 +1,13 @@
+package com.jaiminf.naturaldisasterreliefapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcome_slide4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_slide4);
+    }
+}
